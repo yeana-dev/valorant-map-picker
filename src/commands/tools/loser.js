074e137a1eedaaefacb11bee71d0ffaa760041cb
Yeana Cho <yeana.dev@gmail.com>
2023-01-28 18:37:15 -0600

    Undo

--- src/commands/tools/loser.js
+++ src/commands/tools/loser.js
@@ -1,5 +1,43 @@
 const { SlashCommandBuilder } = require("discord.js");
-const axios = require("axios");
+
+let losers = [
+  {
+    name: "Shinoong",
+    count: 13,
+  },
+  {
+    name: "Yeana",
+    count: 9,
+  },
+  {
+    name: "guhny",
+    count: 12,
+  },
+  {
+    name: "jaydini",
+    count: 9,
+  },
+  {
+    name: "sAnGy",
+    count: 9,
+  },
+  {
+    name: "WaaaSabby",
+    count: 16,
+  },
+  {
+    name: "Z",
+    count: 15,
+  },
+  {
+    name: "BRIVN",
+    count: 5,
+  },
+  {
+    name: "Chirido",
+    count: 0,
+  },
+];
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -10,24 +48,12 @@ module.exports = {
       fetchReply: true,
     });
 
-    let newMsg;
+    let loser = losers[Math.floor(Math.random() * 9)];
+    loser.count++;
 
-    axios
-      .get("https://yeana-discord-bot.herokuapp.com/loser")
-      .then(function (response) {
-        console.log(response);
-        newMsg = response;
-      })
-      .catch(function (error) {
-        console.error(error);
-      });
-
-    // let loser = losers[Math.floor(Math.random() * 9)];
-    // loser.count++;
-
-    // const newMessage = `**${loser.name}** is a loser\n${loser.name}'s been a loser ${loser.count} times`;
+    const newMessage = `**${loser.name}** is a loser\n${loser.name}'s been a loser ${loser.count} times`;
     await interaction.editReply({
-      content: newMsg,
+      content: newMessage,
     });
   },
 };
