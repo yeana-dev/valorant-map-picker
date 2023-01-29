9aa31d7b1077413573aaa7a3dbcba6f38bfc8c89
Yeana Cho <yeana.dev@gmail.com>
2023-01-28 18:25:33 -0600

    Added Axios

--- src/commands/tools/loser.js
+++ src/commands/tools/loser.js
@@ -1,43 +1,5 @@
 const { SlashCommandBuilder } = require("discord.js");
-
-let losers = [
-  {
-    name: "Shinoong",
-    count: 13,
-  },
-  {
-    name: "Yeana",
-    count: 9,
-  },
-  {
-    name: "guhny",
-    count: 12,
-  },
-  {
-    name: "jaydini",
-    count: 9,
-  },
-  {
-    name: "sAnGy",
-    count: 9,
-  },
-  {
-    name: "WaaaSabby",
-    count: 16,
-  },
-  {
-    name: "Z",
-    count: 15,
-  },
-  {
-    name: "BRIVN",
-    count: 5,
-  },
-  {
-    name: "Chirido",
-    count: 0,
-  },
-];
+const axios = require("axios");
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -48,12 +10,23 @@ module.exports = {
       fetchReply: true,
     });
 
+    let newMsg;
+
+    axios.get('/')
+      .then(function (response) {
+        console.log(response);
+        newMsg = response;
+      })
+      .catch(function (error) {
+        console.error(error);
+    })
+
     let loser = losers[Math.floor(Math.random() * 9)];
     loser.count++;
 
-    const newMessage = `**${loser.name}** is a loser\n${loser.name}'s been a loser ${loser.count} times`;
+    // const newMessage = `**${loser.name}** is a loser\n${loser.name}'s been a loser ${loser.count} times`;
     await interaction.editReply({
-      content: newMessage,
+      content: newMsg,
     });
   },
 };
